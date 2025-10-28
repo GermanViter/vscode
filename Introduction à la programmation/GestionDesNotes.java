5f5a73725dad6eb9f259fc25fcab59baa5c7f3fc
GermanViter <german.viter@icloud.com>
2025-10-28 16:20:34 -0400

    Fix: mise à jour des fichiers de tri + gestion des notes

--- Introduction à la programmation/GestionDesNotes.java
+++ Introduction à la programmation/GestionDesNotes.java
@@ -2,35 +2,64 @@ import java.util.Scanner;
 
 public class GestionDesNotes {
     public static void main(String[] args) {
-        double[] notes = new double[30];
-        int compteur = 0;
-
-    }
-
-    public static void afficherMenu(int choix) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Combiens d'étudiants ? ");
+        int nbrDEleves = scan.nextInt();
+        int somme = 0;
 
-        System.out.println("=== Gestionnaire de notes ===");
-        System.out.println("[1] Ajouter une note");
-        System.out.println("[2] Afficher le bulletin");
-        System.out.println("[3] Calculer la moyenne");
-        System.out.println("[4] Afficher la meilleure et la pire note");
-        System.out.println("[5] Quitter");
-        System.out.print("Votre choix : ");
-        choix = scan.nextInt();
+        int[] eleves = new int[nbrDEleves];
+        System.out.println("Entrez les notes de vos élèves : ");
+        for (int i = 0; i < eleves.length; i++) {
+            eleves[i] = Integer.valueOf(scan.next());
+            somme += eleves[i];
+        }
+        double moyenne = somme / (double) eleves.length;
+        quickSort(eleves, 0, eleves.length - 1);
+        System.out.print("Notes triés : ");
+        afficherNotes(eleves);
+        System.out.printf("Moyenne : %.1f\n", moyenne);
+        System.out.printf("Note la plus basse : %d\n", eleves[0]);
+        System.out.printf("Note la plus haute : %d\n", eleves[eleves.length - 1]);
+
     }
 
-    public static void ajouterNote(double[] notes, int compteur) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Entrez une note : ");
-        double note = scan.nextFloat();
-
-        compteur++;
-        notes[compteur] = note;
+    public static void afficherNotes(int[] nums) {
+        System.out.print("[");
+        for (int i = 0; i < nums.length - 1; i++) {
+            System.out.print(nums[i] + ", ");
+        }
+        System.out.print(nums[nums.length - 1] + "]\n");
     }
-    public static void afficherBulletin(double[] notes, int compteur) {
-        System.out.println("Bulletin");
-        
+
+    public static int[] quickSort(int[] nums, int debut, int fin) {
+        if (debut < fin) {
+            int indexPivot = partitions(nums, debut, fin);
+
+            quickSort(nums, debut, indexPivot - 1);
+            quickSort(nums, indexPivot + 1, fin);
+        }
+        return nums;
+    }
+
+    public static int partitions(int[] nums, int debut, int fin) {
+        int pivot = nums[fin];
+        int i = debut - 1;
+
+        for (int j = debut; j < fin; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+
+        swap(nums, i + 1, fin);
+
+        return i + 1;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
